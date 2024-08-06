c26d61a21c1c8ab271b136a80e05e913012b3587
CrunchyBreadcrumbs <144739569+CrunchyBreadcrumbs@users.noreply.github.com>
2024-08-06 10:31:33 +0200

    Create first visual draft with HTML and CSS

---
+++ script.js
@@ -0,0 +1,16 @@
+const addBtn = document.querySelector('#add');
+const subBtn = document.querySelector('#subtract');
+const multBtn = document.querySelector('#multiply');
+const divBtn = document.querySelector('#divide');
+
+// function add (num){
+//     return num + addEventListener(assign second num)
+// };
+
+let firstNum;
+let operator;
+let secondNum;
+
+function operate(opr, num1, num2){
+    
+}
